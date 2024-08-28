8ec3694be3cdbcd68e2d07607ea936af6ad7d955
hza1128 <141570215+hza1128@users.noreply.github.com>
2024-08-28 12:44:59 +0800

    Update install_r4s_fans.sh

--- install_r4s_fans.sh
+++ install_r4s_fans.sh
@@ -4,8 +4,8 @@ cd /usr/bin/
 rm ./fa-fancontrol.sh
 rm ./fa-fancontrol-direct.sh
 
-wget https://raw.githubusercontent.com/ZiGmaX809/R4S_Fans/main/fa-fancontrol.sh
-wget https://raw.githubusercontent.com/ZiGmaX809/R4S_Fans/main/fa-fancontrol-direct.sh
+wget https://raw.githubusercontent.com/hza1128/R4S_Fans/main/fa-fancontrol.sh
+wget https://raw.githubusercontent.com/hza1128/R4S_Fans/main/fa-fancontrol-direct.sh
 
 chmod 777 ./fa-fancontrol.sh
 chmod 777 ./fa-fancontrol-direct.sh
